7537a46f67defd9d688116bae9e8fc1f36cbb046
Alcene <alcene@live.fr>
2025-03-27 06:32:09 +0100

    2025-03-24 06:32

---
+++ Cours_01/01_Structure_d_un_programe.cpp
@@ -0,0 +1,11 @@
+//Importation des biblothèques.
+#include <iostream>
+
+
+//main() est ce qui s'excécute quand nous exécutons le programe.
+int main()
+{
+
+	//On retourne 0, ce qui indique à l'utilisateur, que le programe s'est terminé avec succès
+	return 0;
+}
---
+++ Cours_02/02_affichage.cpp
@@ -0,0 +1,14 @@
+//Bibliothèque contenant std::cout
+#include <iostream>
+
+int main()
+{
+	//Affichage avec std::cout
+	std::cout << "Hello World !" << std::endl;
+
+
+
+	return 0;
+}
+
+//Résultat attendu : Hello World !
