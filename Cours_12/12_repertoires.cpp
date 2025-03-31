4b254b2aa1f1f3dabb8ccc2a5f693dae36a04fc6
Alcene <alcene@live.fr>
2025-03-31 22:07:38 +0200

    2025-03-31 22:05

--- Cours_12/12_repertoires.cpp
+++ Cours_12/12_repertoires.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {
+
 	Personne p1("Alcene", 37);
 	p1.affichagePersonne();
 
@@ -12,8 +13,11 @@ int main()
 	Personne* p2 = new Personne("Peach", 30);
 	p2->affichagePersonne();
 
+	Personne *p3 = new Personne("Alice", 22);
+
 
 	delete p2;
+	delete p3;
 
 	return 0;
 }
